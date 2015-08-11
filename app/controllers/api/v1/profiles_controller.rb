3f0bfb02faf0bc735418623a70c4e3f0a9fa2274
Paul Harker <harkerp@gmail.com>
2015-08-11 18:01:49 +0100

    apitest

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -1,9 +1,23 @@
 module Api
   module V1
     class ProfilesController < ApplicationController
+
       def index
         @profiles = Profile.all
         render json: @profiles
+      end
+
+      def create
+        profile = Profile.new(profile_params)
+        if profile.save
+          render json: { hello: 'goodbye' }.to_json
+        end
+      end
+      
+      private
+
+      def profile_params
+        params.permit(:email, :name)
       end
     end
   end
